22793efe792840119dc55673ab10dd78eb35dece
Roberto Jiménez <roberjmenez72@gmail.com>
2020-07-31 10:13:24 -0500

    Arreglo Correccion

--- Emprendimiento/js/codigo.js
+++ Emprendimiento/js/codigo.js
@@ -14,3 +14,23 @@ function consultaAPI(){
     });
 }
 
+
+
+
+/* Carrusel Clientes */
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
