37d282af4c070953e92dc1c26cf10fc1f63fe908
Jose Fernelly <jfernelly@gmail.com>
2020-07-31 10:46:44 -0500

    Cambios

--- Emprendimiento/js/codigo.js
+++ Emprendimiento/js/codigo.js
@@ -3,11 +3,12 @@ $(document).ready(function(){
 });
 
 function consultaAPI(){
-    const covid = "https://services.arcgis.com/BQTQBNBsmxjF8vus/ArcGIS/rest/services/Colombia_COVID19V/FeatureServer/6/query?where=1%3D1&outFields=TOTAL_CASOS&outSR=4326&f=json"
+    const covid = "https://services.arcgis.com/BQTQBNBsmxjF8vus/ArcGIS/rest/services/Colombia_COVID19V/FeatureServer/6/query?where=1%3D1&outFields=TOTAL_CASOS,FECHA_ACTUALIZACION&outSR=4326&f=json"
 
     $.get( covid, function (data) {
         consulta = data;
         console.log(consulta);
+        personajes.forEach(element => {
 
     
 
@@ -34,3 +35,50 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
 })
+
+
+
+
+/* Efecto Scroll */
+ScrollReveal().reveal('.navbar', {
+    distance: '100%',
+    origin: 'top',
+  });
+  
+
+  ScrollReveal().reveal('.barra', {
+    distance: '100%',
+    origin: 'top',
+  });
+
+  ScrollReveal().reveal('.nosotros', {
+    distance: '100%',
+    origin: 'bottom',
+    delay: 600,
+  });
+  
+
+  ScrollReveal().reveal('#iluminacion', {
+    distance: '100%',
+    origin: 'left',
+    delay: 600,
+  });
+
+  ScrollReveal().reveal('#mobiliario', {
+    distance: '100%',
+    origin: 'right',
+    delay: 600,
+  });
+
+  ScrollReveal().reveal('#cocina', {
+    distance: '100%',
+    origin: 'left',
+    delay: 800,
+  });
+
+  ScrollReveal().reveal('#interior', {
+    distance: '100%',
+    origin: 'right',
+    delay: 800,
+  });
+  
