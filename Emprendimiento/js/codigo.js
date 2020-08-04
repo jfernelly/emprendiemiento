594727f6803bbaee47fe36cd77722189cca759e0
Jose Fernelly <jfernelly@gmail.com>
2020-08-04 10:02:58 -0500

    Mensaje covid

--- Emprendimiento/js/codigo.js
+++ Emprendimiento/js/codigo.js
@@ -2,22 +2,7 @@ $(document).ready(function(){
     consultaAPI();
 });
 
-function consultaAPI(){
-    const covid = "https://services.arcgis.com/BQTQBNBsmxjF8vus/ArcGIS/rest/services/Colombia_COVID19V/FeatureServer/6/query?where=1%3D1&outFields=TOTAL_CASOS,FECHA_ACTUALIZACION&outSR=4326&f=json"
 
-    $.get( covid, function (data) {
-        //consulta = data;
-        //console.log(data);
-        console.log(data.attributes.pop());
-        
-
-        
-        
-
-    
-
-    });
-}
 
 
 
@@ -86,4 +71,36 @@ ScrollReveal().reveal('.navbar', {
     delay: 800,
   });
 
+  ScrollReveal().reveal('.alerta', {
+    distance: '100%',
+    origin: 'top',
+  });
+
+
+  function consultaAPI(){
+    const covid = 'https://services.arcgis.com/BQTQBNBsmxjF8vus/ArcGIS/rest/services/Colombia_COVID19V/FeatureServer/6/query?where=1%3D1&outFields=TOTAL_CASOS,FECHA_ACTUALIZACION,TOTAL_RECUPERADOS,NUEVOS_CASOS&outSR=4326&f=json&t='+Date.now();
+
+    $.get( covid, function (data) {
+        casos = JSON.parse(data) //de JSON a objeto
+        console.log(casos.features[casos.features.length - 1].attributes);
+        $('#iContagios').text(casos.features[casos.features.length - 1].attributes.TOTAL_CASOS);
+        $('#iRecuperados').text(casos.features[casos.features.length - 1].attributes.TOTAL_RECUPERADOS);
+        $('#iNuevosCasos').text(casos.features[casos.features.length - 1].attributes.NUEVOS_CASOS);
+        //console.log(data);
+        
+        //console.log(data.features[0].attributes.TOTAL_CASOS);   
+        //console.log(data.features.attributes.TOTAL_CASOS.pop());
+        //arreglo = data.features; 
+        //console.log(arreglo)
+        /*
+
+        for(var i = 0; i < arreglo.length; i++) {
+            console.log(arreglo[i])
+        }*/
+
+        /*
+            const covid = 'https://services.arcgis.com/BQTQBNBsmxjF8vus/ArcGIS/rest/services/Colombia_COVID19V/FeatureServer/6/query?where=1%3D1&outFields=TOTAL_CASOS,FECHA_ACTUALIZACION&outSR=4326&f=json&t='+Date.now();
+            */
+    });
+}
   
